5f0aa56c106217af0171682154e4f2ed9f2b5b7b
Kyle93 <migeman@hotmail.com>
2013-05-15 00:42:39 +0200

    Update EventPage.java

--- EventPage.java
+++ EventPage.java
@@ -2,8 +2,11 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -11,12 +14,10 @@ public class EventPage extends javax.swing.JPanel
 {
 
     /**
-   * Event screen
+	 * Event screen
 	 * Card 5
 	 */
 	private static final long serialVersionUID = 1L;
-	private javax.swing.JButton nextButton;
-    private javax.swing.JLabel textLabel;
     private JPanel contentPane;
 
     public EventPage(JPanel cp) 
@@ -27,30 +28,81 @@ public class EventPage extends javax.swing.JPanel
 
     private void initComponents() 
     {
-
-        setOpaque(true);
-        setBackground(Color.RED.darker().darker());
-        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        textLabel = new JLabel("this is the fith card", JLabel.CENTER);
-        textLabel.setForeground(Color.WHITE);
-        nextButton = new javax.swing.JButton();
-
-        nextButton.setText("To start");
-        nextButton.addActionListener(new ActionListener() 
+    	setOpaque(true);
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		setBackground(Color.LIGHT_GRAY);
+		setLayout(null);
+		
+		JButton btnLogOut = new JButton("Log out");
+		btnLogOut.setBounds(500, 10, 89, 23);
+		btnLogOut.addActionListener(new ActionListener()
         {
-            public void actionPerformed(ActionEvent evt) 
+            @Override
+            public void actionPerformed(ActionEvent ae)
             {
-                jButton1ActionPerformed(evt);
+                logoutButton(ae);
             }
         });
-
-        add(textLabel);
-        add(nextButton);
+		add(btnLogOut);
+		
+		JButton btnCreateEvent = new JButton("Create event");
+		btnCreateEvent.setBounds(420, 366, 120, 23);
+		btnCreateEvent.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent ae)
+            {
+                createButton(ae);
+            }
+        });
+		add(btnCreateEvent);
+		
+		JButton btnCancelEvent = new JButton("Go back");
+		btnCancelEvent.setBounds(240, 366, 120, 23);
+		btnCancelEvent.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent ae)
+            {
+                cancelButton(ae);
+            }
+        });
+		add(btnCancelEvent);
+		
+		JButton btnFriendsPage = new JButton("Friends Page");
+		btnFriendsPage.setBounds(60, 366, 120, 23);
+		btnFriendsPage.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent ae)
+            {
+                friendsButton(ae);
+            }
+        });
+		add(btnFriendsPage);
     }
-
-    private void jButton1ActionPerformed(ActionEvent evt) 
+    
+    private void logoutButton(ActionEvent ae)
     {
-        CardLayout layout = (CardLayout) contentPane.getLayout();
+        CardLayout layout = (CardLayout)contentPane.getLayout();
+        layout.show(contentPane, "login");
+    }
+    
+    private void createButton(ActionEvent ae)
+    {
+        CardLayout layout = (CardLayout)contentPane.getLayout();
+        layout.show(contentPane, "createEvent");
+    }
+    
+    private void cancelButton(ActionEvent ae)
+    {
+        CardLayout layout = (CardLayout)contentPane.getLayout();
+        layout.show(contentPane, "eventPage");
+    }
+    
+    private void friendsButton(ActionEvent ae)
+    {
+        CardLayout layout = (CardLayout)contentPane.getLayout();
         layout.show(contentPane, "friendPage");
     }
 }
